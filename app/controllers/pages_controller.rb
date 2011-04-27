ba6bf015c02660ad6d96f455ddaf29d5e9f47cb7
Lisa Maliphol <lisa.maliphol@gmail.com>
2011-04-27 18:22:38 -0400

    Removed default Rails page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+
   def home
     @title = "Home"
   end
@@ -14,4 +15,5 @@ class PagesController < ApplicationController
   def help
     @title = "Help"
   end
+
 end
